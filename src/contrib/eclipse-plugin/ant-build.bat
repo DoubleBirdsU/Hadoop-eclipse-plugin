48e17d50ef4b33822deba5b3904bf9767a47aeff
doublebirdsu <doublebirdsu@gmail.com>
2018-10-17 15:18:33 +0800

    修改build.xml文件

--- src/contrib/eclipse-plugin/ant-build.bat
+++ src/contrib/eclipse-plugin/ant-build.bat
@@ -1,3 +1,3 @@
-@cd /d D:\HadoopEclipse\hadoop2x-eclipse-plugin-master\src\contrib\eclipse-plugin
-del /q /s D:\HadoopEclipse\hadoop2x-eclipse-plugin-master\build\contrib
-ant jar -Dversion=2.8.5 -Declipse.home=D:\HadoopEclipse\EclipseLuna -Dhadoop.home=D:\HadoopEclipse\hadoop-2.8.5
+@cd /d D:\HadoopEclipse\Hadoop-eclipse-plugin\src\contrib\eclipse-plugin
+del /q /s D:\HadoopEclipse\Hadoop-eclipse-plugin\build\contrib
+ant jar -Dversion=2.8.5 -Declipse.home=D:\HadoopEclipse\eclipse -Dhadoop.home=D:\HadoopEclipse\hadoop-2.8.5

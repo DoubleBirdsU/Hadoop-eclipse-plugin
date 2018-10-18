f12f66a53be5792ef047cb9dc19135ef4aa74090
doublebirdsu <doublebirdsu@gmail.com>
2018-10-19 07:47:00 +0800

    删除src/contrib/eclipse-plugin/ant-build.bat文件

--- src/contrib/eclipse-plugin/ant-build.bat
+++
@@ -1,3 +0,0 @@
-@cd /d D:\HadoopEclipse\Hadoop-eclipse-plugin\src\contrib\eclipse-plugin
-del /q /s D:\HadoopEclipse\Hadoop-eclipse-plugin\build\contrib
-ant jar -Dversion=2.8.5 -Declipse.home=D:\HadoopEclipse\eclipse -Dhadoop.home=D:\HadoopEclipse\hadoop-2.8.5
